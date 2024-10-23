ff75cb658dafd40eb25eaefd0009d5795976898a
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2024-10-23 14:46:56 -0300

    elementosOrdenados

--- funnystyles/src/components/Opciones.jsx
+++ funnystyles/src/components/Opciones.jsx
@@ -2,8 +2,19 @@ import React from "react";
 
 function Opciones(){
     return (
-    <div>
+    <div id="orden">
         <h1>Soy el enlace opciones</h1>
+      
+        <div id="container">
+        <div><h2>Soy el contenedor mayor</h2></div>
+        <div>
+            <div className="contenedorS">Esta es la caja pequeña</div>
+            <div className="contenedorS">Esta es la caja pequeña</div>
+            <div className="contenedorS">Esta es la caja pequeña</div>
+            <div className="contenedorS">Esta es la caja pequeña</div>
+            <div className="contenedorS">Esta es la caja pequeña</div>
+            <div className="contenedorS">Esta es la caja pequeña</div>
+        </div></div>
     </div>)
 }
 
